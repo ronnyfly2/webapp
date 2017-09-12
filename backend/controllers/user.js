a14c05fabe3bdfe122de18f7f63f8ec7713996d7
Ronny Cabrera Sinarahua <ronnythefly7@gmail.com>
2017-09-12 15:13:02 -0500

    revise register for emaail repeat

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -21,32 +21,42 @@ function saveUser(req, res){
     user.role = 'ROLE_USER';
     user.image= 'null';
 
-    if(params.password){
-        //encriptar y guardar datos
-        bCrypt.hash(params.password, null, null, function(err,hash){
-            user.password = hash;
-            if(user.name != null && user.lastName != null && user.email != null){
-                //guardar usuario en la base de datos
-                user.save((err, userStored)=>{
-                    if(err){
-                        res.status(500).send({message:'hubo un error al guardar el usuario'});
+    User.findOne({email: user.email.toLowerCase()}, (err, userRepeat)=>{
+        console.log(userRepeat)
+        if(!userRepeat){
+            if(params.password){
+                //encriptar y guardar datos
+                bCrypt.hash(params.password, null, null, function(err,hash){
+                    user.password = hash;
+                    if(user.name != null && user.lastName != null && user.email != null){
+                        //guardar usuario en la base de datos
+                        user.save((err, userStored)=>{
+                            if(err){
+                                res.status(500).send({message:'hubo un error al guardar el usuario'});
+                            }else{
+                                if(!userStored){
+                                    res.status(404).send({message:'no se registro el usuario'});
+                                }else{
+                                    res.status(200).send({user:userStored });
+                                }
+                            }
+                        })
                     }else{
-                        if(!userStored){
-                            res.status(404).send({message:'no se registro el usuario'});
-                        }else{
-                            res.status(200).send({user:userStored });
-                        }
+                        res.status(200).send({message:'rellena los campos'});
                     }
-                })
+                });
             }else{
-                res.status(200).send({message:'rellena los campos'});
+                res.status(500).send({message:'introduce contraseña'});
             }
-        });
-    }else{
-        res.status(500).send({message:'introduce contraseña'});
-    }
+        }else{
+            res.status(200).send({message:'el correo ya existe'});
+        }
+    });
 
+    
 }
+
+
 
 module.exports = {
     pruebas,
